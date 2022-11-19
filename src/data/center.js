06f5147fb9a2ce42785ad6909cf4fad0e738dd53
andishe <a.abbasian@pixaldo.com>
2022-11-19 22:42:03 +0330

    updated mock data

---
+++ src/data/center.js
@@ -0,0 +1,210 @@
+export const centerData = [
+    {
+      "name": "State",
+      "value": [
+        "{category: \"men's clothing\", description: \"Your per…}",
+        "{category: \"men's clothing\", description: \"Slim-fit…}",
+        "{category: \"men's clothing\", description: \"great ou…}",
+        "{category: \"men's clothing\", description: \"The colo…}",
+        "{category: \"jewelery\", description: \"From our Legen…}",
+        "{category: \"jewelery\", description: \"Satisfaction G…}",
+        "{category: \"jewelery\", description: \"Classic Create…}",
+        "{category: \"jewelery\", description: \"Rose Gold Plat…}",
+        "{category: \"electronics\", description: \"USB 3.0 and…}",
+        "{category: \"electronics\", description: \"Easy upgrad…}",
+        "{category: \"electronics\", description: \"3D NAND fla…}",
+        "{category: \"electronics\", description: \"Expand your…}",
+        "{category: \"electronics\", description: \"21. 5 inche…}",
+        "{category: \"electronics\", description: \"49 INCH SUP…}",
+        "{category: \"women's clothing\", description: \"Note:T…}",
+        "{category: \"women's clothing\", description: \"100% P…}",
+        "{category: \"women's clothing\", description: \"Lightw…}",
+        "{category: \"women's clothing\", description: \"95% RA…}",
+        "{category: \"women's clothing\", description: \"100% P…}",
+        "{category: \"women's clothing\", description: \"95%Cot…}"
+      ],
+      "subHooks": [],
+      "hookSource": {
+        "lineNumber": 2,
+        "functionName": "Fi",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+        "columnNumber": 340984
+      }
+    },
+    {
+      "name": "State",
+      "value": [],
+      "subHooks": [],
+      "hookSource": {
+        "lineNumber": 2,
+        "functionName": "Fi",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+        "columnNumber": 341030
+      }
+    },
+    {
+      "name": "Effect",
+      "value": "ƒ () {}",
+      "subHooks": [],
+      "hookSource": {
+        "lineNumber": 2,
+        "functionName": "Fi",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+        "columnNumber": 341074
+      }
+    },
+    {
+      "name": "",
+      "subHooks": [
+        {
+          "name": "",
+          "subHooks": [
+            {
+              "name": "s",
+              "subHooks": [
+                {
+                  "name": "Context",
+                  "value": {
+                    "store": "{@@observable: ƒ v() {}, dispatch: ƒ () {}, getStat…}",
+                    "subscription": "{addNestedSub: ƒ addNestedSub() {}, getListeners: ƒ…}"
+                  },
+                  "subHooks": [],
+                  "hookSource": {
+                    "lineNumber": 2,
+                    "functionName": "s",
+                    "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                    "columnNumber": 214748
+                  }
+                }
+              ],
+              "hookSource": {
+                "lineNumber": 2,
+                "columnNumber": 216908,
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+              }
+            }
+          ],
+          "hookSource": {
+            "lineNumber": 2,
+            "columnNumber": 217045,
+            "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+          }
+        }
+      ],
+      "hookSource": {
+        "lineNumber": 2,
+        "columnNumber": 341224,
+        "functionName": "Fi",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+      }
+    },
+    {
+      "name": "",
+      "subHooks": [
+        {
+          "name": "s",
+          "subHooks": [
+            {
+              "name": "Context",
+              "value": {
+                "store": "{@@observable: ƒ v() {}, dispatch: ƒ () {}, getStat…}",
+                "subscription": "{addNestedSub: ƒ addNestedSub() {}, getListeners: ƒ…}"
+              },
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": 2,
+                "functionName": "s",
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                "columnNumber": 214748
+              }
+            }
+          ],
+          "hookSource": {
+            "lineNumber": 2,
+            "columnNumber": 215057,
+            "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+          }
+        },
+        {
+          "name": ".useSyncExternalStoreWithSelector",
+          "subHooks": [
+            {
+              "name": "Ref",
+              "value": {
+                "hasValue": true,
+                "value": "{name: \"\", type: \"None\"}"
+              },
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": 2,
+                "functionName": "t.useSyncExternalStoreWithSelector",
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                "columnNumber": 208654
+              }
+            },
+            {
+              "name": "Memo",
+              "value": [
+                "ƒ () {}",
+                "ƒ () {}"
+              ],
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": 2,
+                "functionName": "t.useSyncExternalStoreWithSelector",
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                "columnNumber": 208745
+              }
+            },
+            {
+              "name": "SyncExternalStore",
+              "value": {
+                "type": "None",
+                "name": ""
+              },
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": null,
+                "functionName": null,
+                "fileName": null,
+                "columnNumber": null
+              }
+            },
+            {
+              "name": "Effect",
+              "value": "ƒ () {}",
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": 2,
+                "functionName": "t.useSyncExternalStoreWithSelector",
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                "columnNumber": 209091
+              }
+            }
+          ],
+          "hookSource": {
+            "lineNumber": 2,
+            "columnNumber": 215109,
+            "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+          }
+        }
+      ],
+      "hookSource": {
+        "lineNumber": 2,
+        "columnNumber": 341230,
+        "functionName": "Fi",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+      }
+    },
+    {
+      "name": "Effect",
+      "value": "ƒ () {}",
+      "subHooks": [],
+      "hookSource": {
+        "lineNumber": 2,
+        "functionName": "Fi",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+        "columnNumber": 341284
+      }
+    }
+  ]
---
+++ src/data/left.js
@@ -0,0 +1,254 @@
+export const leftData  = [
+    {
+      "name": "State",
+      "value": [
+        {
+          "address": {
+            "geolocation": "{lat: \"-37.3159\", long: \"81.1496\"}",
+            "city": "kilcoole",
+            "street": "new road",
+            "number": 7682,
+            "zipcode": "12926-3874"
+          },
+          "id": 1,
+          "email": "john@gmail.com",
+          "username": "johnd",
+          "password": "m38rmF$",
+          "name": {
+            "firstname": "john",
+            "lastname": "doe"
+          },
+          "phone": "1-570-236-7033",
+          "__v": 0
+        },
+        "{__v: 0, address: {…}, email: \"morrison@gmail.com\",…}",
+        "{__v: 0, address: {…}, email: \"kevin@gmail.com\", id…}",
+        "{__v: 0, address: {…}, email: \"don@gmail.com\", id: …}",
+        "{__v: 0, address: {…}, email: \"derek@gmail.com\", id…}",
+        "{__v: 0, address: {…}, email: \"david_r@gmail.com\", …}",
+        {
+          "address": {
+            "geolocation": "{lat: \"10.3456\", long: \"20.6419\"}",
+            "city": "fresno",
+            "street": "saddle st",
+            "number": 1342,
+            "zipcode": "96378-0245"
+          },
+          "id": 7,
+          "email": "miriam@gmail.com",
+          "username": "snyder",
+          "password": "f238&@*$",
+          "name": {
+            "firstname": "miriam",
+            "lastname": "snyder"
+          },
+          "phone": "1-123-943-0563",
+          "__v": 0
+        },
+        {
+          "address": {
+            "geolocation": "{lat: \"50.3456\", long: \"10.6419\"}",
+            "city": "mesa",
+            "street": "vally view ln",
+            "number": 1342,
+            "zipcode": "96378-0245"
+          },
+          "id": 8,
+          "email": "william@gmail.com",
+          "username": "hopkins",
+          "password": "William56$hj",
+          "name": {
+            "firstname": "william",
+            "lastname": "hopkins"
+          },
+          "phone": "1-478-001-0890",
+          "__v": 0
+        },
+        "{__v: 0, address: {…}, email: \"kate@gmail.com\", id:…}",
+        "{__v: 0, address: {…}, email: \"jimmie@gmail.com\", i…}"
+      ],
+      "subHooks": [],
+      "hookSource": {
+        "lineNumber": 2,
+        "functionName": "Di",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+        "columnNumber": 342021
+      }
+    },
+    {
+      "name": "State",
+      "value": [],
+      "subHooks": [],
+      "hookSource": {
+        "lineNumber": 2,
+        "functionName": "Di",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+        "columnNumber": 342067
+      }
+    },
+    {
+      "name": "Effect",
+      "value": "ƒ () {}",
+      "subHooks": [],
+      "hookSource": {
+        "lineNumber": 2,
+        "functionName": "Di",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+        "columnNumber": 342111
+      }
+    },
+    {
+      "name": "",
+      "subHooks": [
+        {
+          "name": "",
+          "subHooks": [
+            {
+              "name": "s",
+              "subHooks": [
+                {
+                  "name": "Context",
+                  "value": {
+                    "store": "{@@observable: ƒ v() {}, dispatch: ƒ () {}, getStat…}",
+                    "subscription": "{addNestedSub: ƒ addNestedSub() {}, getListeners: ƒ…}"
+                  },
+                  "subHooks": [],
+                  "hookSource": {
+                    "lineNumber": 2,
+                    "functionName": "s",
+                    "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                    "columnNumber": 214748
+                  }
+                }
+              ],
+              "hookSource": {
+                "lineNumber": 2,
+                "columnNumber": 216908,
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+              }
+            }
+          ],
+          "hookSource": {
+            "lineNumber": 2,
+            "columnNumber": 217045,
+            "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+          }
+        }
+      ],
+      "hookSource": {
+        "lineNumber": 2,
+        "columnNumber": 342258,
+        "functionName": "Di",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+      }
+    },
+    {
+      "name": "",
+      "subHooks": [
+        {
+          "name": "s",
+          "subHooks": [
+            {
+              "name": "Context",
+              "value": {
+                "store": "{@@observable: ƒ v() {}, dispatch: ƒ () {}, getStat…}",
+                "subscription": "{addNestedSub: ƒ addNestedSub() {}, getListeners: ƒ…}"
+              },
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": 2,
+                "functionName": "s",
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                "columnNumber": 214748
+              }
+            }
+          ],
+          "hookSource": {
+            "lineNumber": 2,
+            "columnNumber": 215057,
+            "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+          }
+        },
+        {
+          "name": ".useSyncExternalStoreWithSelector",
+          "subHooks": [
+            {
+              "name": "Ref",
+              "value": {
+                "hasValue": true,
+                "value": "{name: \"\", type: \"None\"}"
+              },
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": 2,
+                "functionName": "t.useSyncExternalStoreWithSelector",
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                "columnNumber": 208654
+              }
+            },
+            {
+              "name": "Memo",
+              "value": [
+                "ƒ () {}",
+                "ƒ () {}"
+              ],
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": 2,
+                "functionName": "t.useSyncExternalStoreWithSelector",
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                "columnNumber": 208745
+              }
+            },
+            {
+              "name": "SyncExternalStore",
+              "value": {
+                "type": "None",
+                "name": ""
+              },
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": null,
+                "functionName": null,
+                "fileName": null,
+                "columnNumber": null
+              }
+            },
+            {
+              "name": "Effect",
+              "value": "ƒ () {}",
+              "subHooks": [],
+              "hookSource": {
+                "lineNumber": 2,
+                "functionName": "t.useSyncExternalStoreWithSelector",
+                "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+                "columnNumber": 209091
+              }
+            }
+          ],
+          "hookSource": {
+            "lineNumber": 2,
+            "columnNumber": 215109,
+            "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+          }
+        }
+      ],
+      "hookSource": {
+        "lineNumber": 2,
+        "columnNumber": 342264,
+        "functionName": "Di",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js"
+      }
+    },
+    {
+      "name": "Effect",
+      "value": "ƒ () {}",
+      "subHooks": [],
+      "hookSource": {
+        "lineNumber": 2,
+        "functionName": "Di",
+        "fileName": "https://front-code-challenge-s11q.vercel.app/static/js/main.c2070500.js",
+        "columnNumber": 342318
+      }
+    }
+  ]
